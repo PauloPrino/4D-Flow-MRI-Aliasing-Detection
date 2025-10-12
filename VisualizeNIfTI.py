54ca059c14c5667c45f5be6554d09ab264208a88
PauloPrino <paulprin2003@gmail.com>
2025-10-12 20:47:49 +0200

    Improvement of the quality of visualisation of NIfTI with scale added

--- VisualizeNIfTI.py
+++ VisualizeNIfTI.py
@@ -1,11 +1,13 @@
 import nibabel as nib
 import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+import numpy as np
 
 def visualize_nifti_slices(nii_gz_path, time_frame, position_x, position_y, position_z):
     """
     nii_gz_path: path to the .nii.gz file
     time_frame: time frame to be visualized (value between 0 and 49, because there are 49 time frames)
-    position_x: position of the sagittal slice
+    position_x: position of the sagittal slice (value between 0 and 143, because there are 144 slices in each direction)
     """
     img = nib.load(nii_gz_path) # loading the NIfTI file
     data = img.get_fdata() # transform the image to a numpy array
@@ -18,7 +20,7 @@ def visualize_nifti_slices(nii_gz_path, time_frame, position_x, position_y, posi
     number_of_frames = data.shape[3]
     print(f"Number of time frames: {number_of_frames}")
     data = data[:, :, :, time_frame] # choose time frame to be viualized
-    print(data[position_x,:,:])
+    #print(data[position_x,:,:])
     print(f"Image shape: {data.shape}")
 
     # We only take one slice in each direction at the position asked
@@ -27,18 +29,21 @@ def visualize_nifti_slices(nii_gz_path, time_frame, position_x, position_y, posi
     slice_z = data[:, :, position_z] # axial slice
 
     fig, axes = plt.subplots(1, 3, figsize=(12, 4))
-    axes[0].imshow(slice_x.T, cmap="gray", origin="lower")
-    axes[0].set_title(f"Axial Slice at poistion z={position_z}")
+    axial_view = axes[0].imshow(slice_x.T, cmap="gray", origin="lower")
+    axes[0].set_title(f"Axial Slice at position z={position_z}") # the pixels on the image are velocities in the direction of z
 
-    axes[1].imshow(slice_y.T, cmap="gray", origin="lower")
-    axes[1].set_title(f"Coronal Slice at position y={position_y}")
+    coronal_view = axes[1].imshow(slice_y.T, cmap="gray", origin="lower")
+    axes[1].set_title(f"Coronal Slice at position y={position_y}") # the pixels on the image are velocities in the direction of x
 
-    axes[2].imshow(slice_z.T, cmap="gray", origin="lower")
-    axes[2].set_title(f"Sagital Slice at position x={position_x}")
+    sagital_view = axes[2].imshow(slice_z.T, cmap="gray", origin="lower")
+    axes[2].set_title(f"Sagital Slice at position x={position_x}") # the pixels on the image are velocities in the direction of y
 
+    fig.colorbar(axial_view, ax=axes[0], label="Pixel value")
+    fig.colorbar(coronal_view, ax=axes[1], label="Pixel value")
+    fig.colorbar(sagital_view, ax=axes[2], label="Pixel value")
     plt.tight_layout()
     plt.show()
 
 nii_path = "Dataset/IRM_BAO_069_1_4D_NIfTI/IRM_BAO_069_1_4D_NIfTI_e3.nii.gz"
-visualize_nifti_slices(nii_path, time_frame=5, position_x=64, position_y=64, position_z=32)
+#visualize_nifti_slices(nii_path, time_frame=5, position_x=64, position_y=64, position_z=32)
 #visualize_nifti_slices(nii_path, time_frame=40, position_x=64, position_y=64, position_z=32)
