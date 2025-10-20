e36359cb50e53f5bbcbe94d7341690b60dc12195
PauloPrino <paulprin2003@gmail.com>
2025-10-20 19:30:20 +0200

    Small change on adapting MRIDataset to be more suit to current data

--- MRIDataset.py
+++ MRIDataset.py
@@ -6,14 +6,14 @@ from PIL import Image
 import torchvision.transforms.v2 as transforms
 
 class MRIDataset(Dataset):
-    def __init__(self, image_dir, mask_dir, data_augmentation): # transform : preprocessing transformations (data augmentation)
-        self.image_dir = image_dir
+    def __init__(self, volumes_3D_dir, mask_dir, data_augmentation): # transform : preprocessing transformations (data augmentation)
+        self.volumes_3D_dir = volumes_3D_dir
         self.mask_dir = mask_dir
         self.data_augmentation = data_augmentation
-        self.image_filenames = os.listdir(image_dir)
+        self.volumes_3D_filenames = os.listdir(volumes_3D_dir)
 
     def __len__(self): # we redefine the len function to be the nbre of images in the dataset
-        return len(self.image_filenames)
+        return len(self.volumes_3D_filenames)
 
     def transform_both(self, image, mask):
         return transforms.Compose([
@@ -37,30 +37,30 @@ class MRIDataset(Dataset):
         ])(image)
     
     def __getitem__(self, idx):
-        img_path = os.path.join(self.image_dir, self.image_filenames[idx])
-        mask_path = os.path.join(self.mask_dir, self.image_filenames[idx]) # we assume that the masks have the same filenames as the images
+        volume_3D_path = os.path.join(self.volumes_3D_dir, self.volumes_3D_filenames[idx])
+        mask_path = os.path.join(self.mask_dir, self.volumes_3D_filenames[idx]) # we assume that the masks have the same filenames as the images
         
-        image = plt.imread(img_path) # loading the image as a numpy array
+        volume_3D = plt.imread(volume_3D_path) # loading the image as a numpy array
         mask = plt.imread(mask_path)
 
         # Normalize and convert to uint8
-        if image.dtype == 'float32' or image.dtype == 'float64':
-            image = (255 * image).astype('uint8')
+        if volume_3D.dtype == 'float32' or volume_3D.dtype == 'float64':
+            volume_3D = (255 * volume_3D).astype('uint8')
         if mask.dtype == 'float32' or mask.dtype == 'float64':
             mask = (255 * mask).astype('uint8')
 
-        image = Image.fromarray(image) # transforming the image to PIL
+        volume_3D = Image.fromarray(volume_3D) # transforming the image to PIL
         mask = Image.fromarray(mask)
 
         if self.data_augmentation: # if there is some data augmentation
-            image, mask = self.transform_both(image, mask) # apply the transformations on both the image and corresponding mask
+            volume_3D, mask = self.transform_both(volume_3D, mask) # apply the transformations on both the image and corresponding mask
 
-        image = self.transform_image(image)
+        volume_3D = self.transform_image(volume_3D)
         mask = self.transform_mask(mask)
 
         #mask = mask.convert("L") # we ensure that mask have only one channel (grayscale)
 
         mask = (mask > 0.5).float() # ensure mask is binary
 
-        return image, mask
+        return volume_3D, mask
 
